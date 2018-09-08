e448808c287f4df2020eb4ca87b44c5b55bd5fa8
yuokada <callistoiv+git@gmail.com>
2018-09-08 22:48:19 +0900

    Add Django-demo code

---
+++ Django-app/demo/views.py
@@ -0,0 +1,18 @@
+# from django.core.serializers import json
+import json
+from django.shortcuts import HttpResponse
+
+
+def hello_world(request, name):
+    greet = "Hello %s from flask!" % name
+    result = {
+        "ResultSet": {
+            "Result": {
+                "Greeting": greet
+            }
+        }
+    }
+
+    response = HttpResponse(json.dumps(result), status=200)
+    response['Content-Type'] = 'application/json'
+    return response
