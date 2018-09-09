6ea29b57dd4e0052fb3d74190b31708dff3e7d2e
yuokada <callistoiv+git@gmail.com>
2018-09-09 23:09:29 +0900

    Add Batch-app

---
+++ Batch-app/README.md
@@ -0,0 +1,5 @@
+## How to get profile.svg
+
+```
+$ sudo py-spy --flame profile.svg -d 20 -r 10000 -- python3.6 scripts.py
+```
---
+++ Batch-app/scripts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import time
+
+def long_function(n):
+    time.sleep(n)
+    return
+
+if __name__ == '__main__':
+    print('Hello')
+    for i in range(120):
+        long_function(1)
